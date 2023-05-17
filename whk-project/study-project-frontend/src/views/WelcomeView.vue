f9e1c9d7fcf772a5bebcc69baa52906ca5ddd305
whk <1260976240@qq.com>
2023-05-17 15:23:54 +0800

    重置密码完成

---
+++ whk-project/study-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1;background-color: dodgerblue">
+      <el-image style="width:100%;height: 100%" fit="cover" src="https://img1.baidu.com/it/u=2463514011,1142503686&fm=253&fmt=auto&app=138&f=JPEG?w=888&h=500"></el-image>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流。</div>
+      <div style="margin-top: 5px">在这里你可以同性交友，因为都是男的，没有学Java的女生。</div>
+    </div>
+    <div style="width: 400px;background-color: white;z-index: 1" >
+      <router-view v-slot="{ Component }">
+        <transition name="el-zoom-in-center" mode="out-in">
+          <component :is="Component" style="height: 100%"/>
+        </transition>
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
